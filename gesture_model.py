c923ae36a9d13e7c91247f8d710824f2088569dd
Kaktus500 <paul.delseith@gmail.com>
2022-10-15 18:09:25 -0500

    Set up video recordig and model training

---
+++ gesture_model.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+
+class Model():
+    def __init__(self) -> None:
+        pass
+
+    def load_model(self):
+        pass
---
+++ train_gesture.py
@@ -0,0 +1,5 @@
+import cv2
+from camera_input import cameraInput
+
+cam = cameraInput()
+cam.capture_video()
