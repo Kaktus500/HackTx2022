f233cb9da9c7e2530fb172a7d15dcd3715f4fe54
Kaktus500 <paul.delseith@gmail.com>
2022-10-15 22:03:24 -0500

    Save training data to csv

--- train_gesture.py
+++ train_gesture.py
@@ -1,3 +1,4 @@
+import csv
 import cv2
 from camera_input import cameraInput
 import pickle
@@ -9,9 +10,10 @@ date = datetime.now().isoformat()
 
 basePath = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
 
-data_path = os.path.join(basePath,'model','keypoint_classifier','data','1234')
+data_path = os.path.join(
+    basePath, "model", "keypoint_classifier", "data", "keypoint.csv"
+)
 
-cam = cameraInput()
+cam = cameraInput(csv_path=data_path)
 cam.capture_video()
-with open(data_path, 'wb') as f:
-    pickle.dump(cam.capture, f)
+cam.process_capture()
